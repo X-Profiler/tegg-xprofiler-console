92f01900ebc97dd99ad424035f086dc11c830dbb
hyj1991 <yeekwanvong@gmail.com>
2022-07-03 19:50:40 +0800

    init xprofiler-console by tegg@next

---
+++ scan.ts
@@ -0,0 +1,18 @@
+import 'reflect-metadata';
+import fs from 'fs/promises';
+import path from 'path';
+import { Scanner } from '@xprofiler/tegg';
+
+export async function scan() {
+  const baseDir = __dirname;
+
+  const scanner = new Scanner({
+    needWriteFile: false,
+    configDir: 'config',
+    extensions: ['.ts'],
+  });
+  const manifest = await scanner.scan(baseDir);
+  await fs.writeFile(path.join(baseDir, 'manifest.json'), JSON.stringify(manifest, null, 2))
+}
+
+scan();
---
+++ start.ts
@@ -0,0 +1,15 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { Application } from '@xprofiler/tegg';
+
+export async function start(env: string) {
+  const baseDir = __dirname;
+  const manifest = JSON.parse(await fs.readFile(path.join(baseDir, 'manifest.json'), 'utf8'));
+
+  const app = new Application();
+  await app.load(manifest[env], __dirname);
+  await app.run();
+
+  return app;
+}
+
